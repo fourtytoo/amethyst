d9366a6593b72117e6069f87fb70d78519538e23
Walter Pelissero <wcp@book.home.lan>
2021-02-23 19:58:58 +0100

    Clean up project file

--- project.clj
+++ project.clj
@@ -10,15 +10,12 @@
                  [ring "1.8.2"]
                  [metosin/jsonista "0.3.0"] ; override muuntaja's
                  [metosin/compojure-api "2.0.0-alpha30"]
-                 ;; [metosin/spec-tools "0.10.5"]
-                 ;; [metosin/ring-swagger-ui "3.36.0"]
-                 ;; [metosin/ring-swagger "0.26.2"]
                  ;; [org.quartz-scheduler/quartz "2.2.3"]
                  [clojurewerkz/quartzite "2.1.0"]
                  [com.draines/postal "2.0.4"]]
-  :ring {:handler amethyst.handler/app}
-  :main         amethyst.server
-  :aot          [amethyst.server]
+  :ring {:handler amethyst.handler/handler}
+  :main amethyst.server
+  :aot [amethyst.server]
   :uberjar-name "amethyst.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
                    :plugins [[lein-ring "0.12.5"]]
